1e1287f5adb92e7cb72c9e07e5f52a4715217ac0
linwenjun <linwenjun@163.com>
2016-08-10 22:44:55 +0800

    add istanbul

--- spec/routers/app_spec.js
+++ spec/routers/app_spec.js
@@ -1,8 +1,21 @@
 import app from '../../server';
-import supertest from 'supertest';
+import request from 'supertest';
 
 describe('get /demo', ()=> {
-  it('should return obj', ()=> {
-    expect(true).toBeTruthy();
+  it('should return obj', (done)=> {
+    request(app)
+        .get('/demo')
+        .expect({
+          error: null,
+          data: 12334
+        })
+        .end((err)=> {
+          if(err) {
+            done.fail(err);
+          } else {
+            done();
+          }
+        })
+
   })
 });
