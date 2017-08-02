2e1a8f011a7c2a2de5c44e12c56c55a854f95755
yujuan <15809216911@163.com>
2017-08-02 18:29:32 +0800

    Add get all usermessages

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'hhxx',//自己的密码
-  database: 'User'//自己创建的数据库名
+  database: 'user'//自己创建的数据库名:user数据库里的User
 });
 //连接数据库
 connection.connect(function (err) {
@@ -26,15 +26,16 @@ connection.connect(function (err) {
   }
   console.log("connection id" + connection.threadId);
 });
-/*//增加一个user
- app.post('/insert-user', function (req, res) {
- connection.query('insert into `users` set ?',req.body,
- function (err, result) {
- if (err) throw err;
- res.send('User added to database with ID: ' + result.insertId);
- }
- );
- });*/
+
+/*获取用户信息*/
+app.get('/all', (req, res)=> {
+  connection.query('select *from User', (err, results)=> {
+    if (err) {
+      throw err;
+    }
+    res.send(results);
+  })
+});
 app.listen(3000, function () {
   console.log('server started at http://localhost:3000'); // eslint-disable-line no-console
 });
