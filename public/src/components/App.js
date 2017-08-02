b319226110f6c8deb1abde8d6183359931fd2d71
yujuan <15809216911@163.com>
2017-08-02 10:39:21 +0800

    hello output

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
-import AddTodo from '../containers/AddTodo';
-import ResultList from './ResultList';
-import PeopleList from '../containers/PeopleList';
 
 class App extends Component {
 
   render() {
     return (
         <div>
-          <PeopleList />
-          <AddTodo />
-          <ResultList />
+          hello
         </div>
     );
   }
