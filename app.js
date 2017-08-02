8f4855e3d82eb3e9a4a806be1af574b2328c8f69
yujuan <15809216911@163.com>
2017-08-02 11:25:31 +0800

    config app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import mysql from 'mysql';
 
 const app = express();
-
-/*mongoose.connect('mongodb://localhost/todos');*/
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -13,8 +11,31 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.static('public'));
 
 
-
-app.listen(3000, function() {
+//初始化数据库配置
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'hhxx',//自己的密码
+  database: 'User'//自己创建的数据库名
+});
+//连接数据库
+connection.connect(function (err) {
+  if (err) {
+    console.log("err" + err.stack);
+    return;
+  }
+  console.log("connection id" + connection.threadId);
+});
+/*//增加一个user
+ app.post('/insert-user', function (req, res) {
+ connection.query('insert into `users` set ?',req.body,
+ function (err, result) {
+ if (err) throw err;
+ res.send('User added to database with ID: ' + result.insertId);
+ }
+ );
+ });*/
+app.listen(3000, function () {
   console.log('server started at http://localhost:3000'); // eslint-disable-line no-console
 });
 
